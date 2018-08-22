f268a479d8671ca753e238283946c50450769e0d
Ezra Joseph Cook <ezra.cook@u.northwestern.edu>
2018-08-22 18:54:24 -0500

    assert

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const assert = require('assert');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require("fs");
