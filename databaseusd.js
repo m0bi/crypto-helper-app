b16850d4511006465299a1a8cc822bbb6285bb2c
Ezra Joseph Cook <ezra.cook@u.northwestern.edu>
2018-08-27 18:59:19 -0500

    debug

--- databaseusd.js
+++ databaseusd.js
@@ -5,6 +5,7 @@ var redis = new Redis(process.env.REDIS_URL);
 let usd = resolve.cash();
 
 usd.then((response)=>{
+    console.log(response);
     redis.set('cash', JSON.stringify(response));
     redis.set('cashtime', JSON.stringify(new Date()));
 });
