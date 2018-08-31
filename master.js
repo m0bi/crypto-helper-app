fc6e86a498e26de70ed1048c38c031b31ddbd1c4
Ezra Joseph Cook <ezra.cook@u.northwestern.edu>
2018-08-31 11:32:36 -0500

    outside routes

--- master.js
+++ master.js
@@ -11,18 +11,23 @@ var keys = require("./keys.js");
 require("dotenv").config();
 module.exports = {
     master: async function populate() {
-        try{
-        const LIVE = axios.get('https://headless-api-crypto.herokuapp.com/live');
-        const BOOK = axios.get('https://headless-api-crypto.herokuapp.com/book');
-        const CASH = axios.get('https://headless-api-crypto.herokuapp.com/cash');
+        try {
+            const LIVE = axios.get('https://headless-api-crypto.herokuapp.com/live');
+            const BOOK = axios.get('https://headless-api-crypto.herokuapp.com/book');
+            const CASH = axios.get('https://headless-api-crypto.herokuapp.com/cash');
 
-        const PROMISE = [];
-        PROMISE.push(LIVE);
-        PROMISE.push(BOOK);
-        PROMISE.push(CASH);
-        let result = await Promise.all(PROMISE);
-        return result;
-        } catch(error){
+            const PROMISE = [];
+            PROMISE.push(LIVE);
+            PROMISE.push(BOOK);
+            PROMISE.push(CASH);
+            let result = await Promise.all(PROMISE);
+            const data = [];
+            for (let i = 0; i < result.length; i++) {
+                data.push(result[i].data);
+            }
+            return data;
+
+        } catch (error) {
             console.log(error)
         }
     }
