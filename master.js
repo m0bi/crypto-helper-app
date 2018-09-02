44ede8f55ed14917a844b7733ffc9ceb7765da79
Ezra Joseph Cook <ezra.cook@u.northwestern.edu>
2018-09-02 13:12:49 -0500

    master split

---
+++ master.js
@@ -0,0 +1,43 @@
+(async function master() {
+    const VALS = [];
+    await redis.get('cash').then((result) => {
+        VALS.push(JSON.parse(result));
+    }).catch(err => console.log(err));
+    await redis.get('live').then((result) => {
+        VALS.push(JSON.parse(result));
+    }).catch(err => console.log(err));
+    const RETURN = [];
+    for(key in VALS[1]) {
+      const SPLIT = key.split('/');
+      let returnObject = {};
+      VALS[0].map((value)=>{
+        if(value.id == SPLIT[0]) {
+          returnObject["Lid"] = value.id;
+          returnObject["LdisplayName"] = value.display_name;
+          returnObject["L24hrChange"] = value.cap24hrChange;
+          returnObject["Lprice"] = value.price;
+        }
+        if(value.id == SPLIT[1]) {
+          returnObject["Rid"] = value.id;
+          returnObject["RdisplayName"] = value.display_name;
+          returnObject["R24hrChange"] = value.cap24hrChange;
+          returnObject["Rprice"] = value.price;
+        }
+        if(SPLIT[0] == 'USDT'){
+            returnObject["Lid"] = 'USDT';
+            returnObject["LdisplayName"] = 'Tether';
+            returnObject["L24hrChange"] = 0;
+            returnObject["Lprice"] = 1;
+        }
+        if(SPLIT[1] == 'USDT'){
+            returnObject["Rid"] = 'USDT';
+            returnObject["RdisplayName"] = 'Tether';
+            returnObject["R24hrChange"] = 0;
+            returnObject["Rprice"] = 1;
+        }
+      });
+      returnObject["values"] = VALS[1][key];
+      RETURN.push(returnObject);
+    }
+    redis.set('master', JSON.stringify(RETURN));
+})();
