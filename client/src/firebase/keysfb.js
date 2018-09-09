c55673a17a0c385a94f2e09574d7e95e442a80aa
Ezra <ezra.cook@u.northwestern.edu>
2018-09-09 17:15:44 -0500

    rebuild

--- client/src/firebase/keysfb.js
+++ client/src/firebase/keysfb.js
@@ -1,7 +1,12 @@
 require("dotenv").config({path: '../../.env'});
 
 module.exports = {
-    firebase_api: process.env.REACT_APP_SECRET_CODE
+    firebase_key: process.env.FIREBASE_KEY,
+    firebase_auth: process.env.FIREBASE_AUTH,
+    firebase_url: process.env.FIREBASE_URL,
+    firebase_id: process.env.FIREBASE_ID,
+    firebase_bucket: process.env.FIREBASE_BUCKET,
+    firebase_sender: process.env.FIREBASE_SENDER 
   };
 
   
