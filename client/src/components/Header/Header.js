1f5e4a54c09b434b6df053c6d21bb5c0cad91c8a
Ezra <ezra.cook@u.northwestern.edu>
2018-09-09 17:47:14 -0500

    work damn you

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -13,20 +13,21 @@ const Header = props => (
             <Link to={routes.HOME}>
                 <img className="navbar-brand" src={require("../../img/brand_logo/coin-crusader_logo.svg")} alt="Coin Crusader" /> <span className="brand-name">Coin Crusader</span>
             </Link>
-            <Navigation authUser={props.authUser} />
+            <Navigation />
+            {/* <Navigation authUser={props.authUser} /> */}
         </nav>
     </header>
 )
 
-const Navigation = ({ authUser }) =>
-    <div>
-        {authUser
-            ? <NavigationAuth />
-            : <NavigationNonAuth />
-        }
-    </div>
+// const Navigation = ({ authUser }) =>
+//     <div>
+//         {authUser
+//             ? <NavigationAuth />
+//             : <NavigationNonAuth />
+//         }
+//     </div>
 
-const NavigationAuth = () =>
+const Navigation = () =>
     <div className="nav-links d-flex flex-row justify-content-end">
         <div className="nav-item nav-link">
             <Link to={routes.HOME}>Home</Link>
@@ -42,23 +43,23 @@ const NavigationAuth = () =>
         </div> */}
     </div>
 
-const NavigationNonAuth = () =>
-    <div className="nav-links d-flex flex-row align-items-end">
-        <div className="nav-item nav-link">
-            <Link to={routes.HOME}>Home</Link>
-        </div>
-        <div className="nav-item nav-link">
-            <a href="#overviewContainer" onClick={() => scrollToComponent(this.overviewContainer, { offset: 100, align: 'bottom', duration: 500, ease: 'inExpo' })}>Overview</a>
-        </div>
-        <div className="nav-item nav-link">
-            <Link to="#aboutContainer">About</Link>
-        </div>
-        <div className="nav-item nav-link">
-            <Link to="#newsContainer">Newsfeed</Link>
-        </div>
-        {/* z<div className="nav-item nav-link">
-            <Link to={routes.SIGN_IN}>Sign In</Link>
-        </div> */}
-    </div>
+// const NavigationNonAuth = () =>
+//     <div className="nav-links d-flex flex-row align-items-end">
+//         <div className="nav-item nav-link">
+//             <Link to={routes.HOME}>Home</Link>
+//         </div>
+//         <div className="nav-item nav-link">
+//             <a href="#overviewContainer" onClick={() => scrollToComponent(this.overviewContainer, { offset: 100, align: 'bottom', duration: 500, ease: 'inExpo' })}>Overview</a>
+//         </div>
+//         <div className="nav-item nav-link">
+//             <Link to="#aboutContainer">About</Link>
+//         </div>
+//         <div className="nav-item nav-link">
+//             <Link to="#newsContainer">Newsfeed</Link>
+//         </div>
+//         {/* z<div className="nav-item nav-link">
+//             <Link to={routes.SIGN_IN}>Sign In</Link>
+//         </div> */}
+//     </div>
 
 export { Header, Navigation };
