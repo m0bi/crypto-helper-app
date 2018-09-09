81ed99e3e742f4a906844aa5181080d47caa59b3
Ezra <ezra.cook@u.northwestern.edu>
2018-09-09 17:55:27 -0500

    cards

--- client/src/pages/CardsPage.js
+++ client/src/pages/CardsPage.js
@@ -32,20 +32,20 @@ class CardsPage extends Component {
                     <Card
                         key={i}
                         id={i}
-                        coin={card.leftDisplayName}
-                        coinValue={card.leftPrice}
-                        coinChange={card.left24hrChange}
-                        currency={card.rightDisplayName}
-                        currencyValue={card.rightPrice}
-                        coinChange={card.right24hrChange}
-                        leftHandValue={card.low[2]}
-                        rightHandValue={card.high[2]}
-                        currencyDiff={card.high[2] - card.low[2]}
-                        leftUsdValue={card.low[2] * card.rightPrice}
-                        rightUsdValue={card.high[2] * card.rightPrice}
-                        usdDiff={(card.low[2] * card.rightPrice) - (card.high[2] * card.rightPrice)}
-                        leftExchange={card.low[0]}
-                        rightExchange={card.high[0]}
+                        // coin={card.leftDisplayName}
+                        // coinValue={card.leftPrice}
+                        // coinChange={card.left24hrChange}
+                        // currency={card.rightDisplayName}
+                        // currencyValue={card.rightPrice}
+                        // coinChange={card.right24hrChange}
+                        // leftHandValue={card.low[2]}
+                        // rightHandValue={card.high[2]}
+                        // currencyDiff={card.high[2] - card.low[2]}
+                        // leftUsdValue={card.low[2] * card.rightPrice}
+                        // rightUsdValue={card.high[2] * card.rightPrice}
+                        // usdDiff={(card.low[2] * card.rightPrice) - (card.high[2] * card.rightPrice)}
+                        // leftExchange={card.low[0]}
+                        // rightExchange={card.high[0]}
                     >
                     </Card>
                 ))}
