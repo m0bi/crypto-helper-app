08eab5bb669a145338560be15696deae329a72e2
Ezra <ezra.cook@u.northwestern.edu>
2018-09-09 10:08:43 -0500

    star route

--- server.js
+++ server.js
@@ -712,6 +712,9 @@ app.get("/API/master", (req, res) => {
 // });
 
 
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function () {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
